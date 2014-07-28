d555d5a202c720fd6ed079b7cd73f5d241c1dac2
Li-Wen Yin <super.invincible.sailor.vbn@gmail.com>
2014-07-28 23:07:47 +1200

    changes1

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,20 +1,36 @@
-class dna:
-		def to_rna(self,s):
+class DNA:
+		def __init__(self, s):
+			self.s = s
+			print s
+		def to_rna(s):
+
+			if s == 'G':
+				s = 'C'
+					#c->G
+			if s == 'C':
+				 return'G'
+					#T->A
+			if s== 'T':
+				s = 'A'
+					#A->U
+			if s == 'A':
+				s = 'U'
+				
+			#for i in range(0,len(s)):
+					#G->c
+			#	if s[i] == 'G':
+			#		s[i] = 'C'
+					#c->G
+			#	if s[i] == 'C':
+			#		s[i] = 'G'
+					#T->A
+			#	if s[i] == 'T':
+			#		s[i] = 'A'
+					#A->U
+			#	if s[i] == 'A':
+			#		s[i] = 'U'
 			
-			for i in range(0,len(s)):
-				#G->c
-				if i == 'G':
-					i = 'C'
-				#c->G
-				if i == 'C':
-					i = 'G'
-				#T->A
-				if i == 'T':
-					i = 'A'
-				#A->U
-				if i == 'A':
-					i = 'U'
-				print i
+				return s
 			
 		
 		
