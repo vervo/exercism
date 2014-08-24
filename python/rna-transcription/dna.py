fd127efe614ce6b4e6022ec0fb911002cfaa4f40
Li-Wen Yin <super.invincible.sailor.vbn@gmail.com>
2014-08-24 18:52:01 +1200

    rna-transcription py - complete.

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,36 +1,16 @@
 class DNA:
+		N = {
+			"G" : "C",
+			"C" : "G",
+			"T" : "A",
+			"A" : "U",
+		}
+
 		def __init__(self, s):
 			self.s = s
-			
-		def to_rna(self):
 
-			if self.s == 'G':
-				self.s = 'C'
-					#c->G
-			if self.s == 'C':
-				return'G'
-					#T->A
-			if self.s == 'T':
-				self.s = 'A'
-					#A->U
-			if self.s == 'A':
-				self.s = 'U'
-				
-			#for i in range(0,len(s)):
-					#G->c
-			#	if s[i] == 'G':
-			#		s[i] = 'C'
-					#c->G
-			#	if s[i] == 'C':
-			#		s[i] = 'G'
-					#T->A
-			#	if s[i] == 'T':
-			#		s[i] = 'A'
-					#A->U
-			#	if s[i] == 'A':
-			#		s[i] = 'U'
-			
-			return self.s
-			
-		
-		
+		def to_rna(self):
+			output = ""
+			for i in self.s:
+				output += self.N[i]
+			return output
